8319f7a1a822f5c163f9d7809c1fb100356b5386
G0m1bak0 <lyhenry2004@gmail.com>
2024-05-06 14:06:59 +1200

    Create components folder to store react components

---
+++ frontend/src/app/components/MeetTheTeam.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import styles from './contacts.module.css';
+
+export default function MeetTheTeam() {
+ 
+  return (
+      <div className={styles.MTTBg}>
+        <div className={styles.flex_container}>
+
+        </div>
+      </div>
+  )
+}
