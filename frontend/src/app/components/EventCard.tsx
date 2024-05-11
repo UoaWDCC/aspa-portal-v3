9220888567b3ed2b1970a25a8c2283bc137ffdb4
G0m1bak0 <lyhenry2004@gmail.com>
2024-05-12 00:56:15 +1200

    Added placeholder data and got components setup

---
+++ frontend/src/app/components/EventCard.tsx
@@ -0,0 +1,18 @@
+import styles from "./EventCard.module.css";
+
+// Define Event type
+interface Event {
+  eventName: string;
+  eventDescription: string;
+  imgUrl: string;
+}
+
+export default function EventCard({ event }: { event: Event }) {
+  return (
+    <div className={styles.card}>
+      <img src={event.imgUrl} alt={event.eventName} />
+      <h1>{event.eventName}</h1>
+      <p>{event.eventDescription}</p>
+    </div>
+  );
+}
