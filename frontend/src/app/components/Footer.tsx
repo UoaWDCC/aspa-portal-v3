3a67136e94e3908c0a535d0902458a18f3f4c99c
Henry Ly <lyhenry2004@gmail.com>
2024-05-17 00:39:42 +1200

    Implemented early form of footer

---
+++ frontend/src/app/components/Footer.tsx
@@ -0,0 +1,13 @@
+import styles from "./Footer.module.css";
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.vertcontainer}>
+        <h1>About Us</h1>
+        <p className={styles.p}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+
+    </footer>
+  );
+}
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,14 +1,16 @@
 import styles from "./page.module.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import PastEvents from "./components/PastEvents";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Header />
-      <div className={styles.body}>
+      <div className={styles.inner}>
         <PastEvents />
       </div>
+      <Footer />
     </main>
   );
 }
