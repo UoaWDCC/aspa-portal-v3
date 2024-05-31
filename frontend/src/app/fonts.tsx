fbb104ca13e516594ab9cafa175e6635db8d5bd5
G0m1bak0 <lyhenry2004@gmail.com>
2024-05-31 13:36:23 +1200

    Implemented footer, but footer needs work especially for responsiveness

---
+++ frontend/src/app/fonts.tsx
@@ -0,0 +1,7 @@
+/* 
+Font preloader for web optimization
+Awaiting for font upload to the repo
+*/
+import { Zilla_Slab } from 'next/font/google';
+
+export const zillaSlab = Zilla_Slab({weight: ['400', '700']})
