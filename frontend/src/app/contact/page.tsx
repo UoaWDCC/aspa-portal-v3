24e64e8f774c7ddbd7e8c0c3302eb3dbe4b2cd6f
ylei532 <80793740+ylei532@users.noreply.github.com>
2024-06-25 01:04:48 +1200

    contact us form integrated on contact page

--- frontend/src/app/contact/page.tsx
+++ frontend/src/app/contact/page.tsx
@@ -1,10 +1,15 @@
 
-import TeamList from '../../ui/contact/TeamList'
+import TeamList from '@/ui/contact/TeamList'
+import ContactForm from '@/ui/contact/ContactForm';
+
 export default function Contact() {
   return (
     <div>
       <section id="meet-the-team">
         <TeamList/>
+      </section>
+      <section id="contact-form" style={{paddingBottom: '8em'}}>
+        <ContactForm/>
       </section>
     </div>
   );
--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -2,7 +2,7 @@ import PastEvents from "@/ui/PastEvents";
 
 export default function Home() {
   return (
-    <div style={{outline:'solid 20px yellow' }}>
+    <div>
       <section id="past-events">
         <PastEvents/>
       </section>
