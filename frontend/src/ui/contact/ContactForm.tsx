b1f02a29f833b329254582f0d981f171956400b6
ylei532 <80793740+ylei532@users.noreply.github.com>
2024-06-25 10:01:21 +1200

    bug

--- frontend/src/ui/contact/ContactForm.tsx
+++ frontend/src/ui/contact/ContactForm.tsx
@@ -1,5 +1,7 @@
+'use client';
+
 import React from 'react';
-import {useState} from 'react';
+import { useState } from 'react';
 import styles from './contacts.module.css';
 
 function ContactForm(){
