bd80d234c4c2472c679f6168389b5c27f695a690
Yoyo <xche558@aucklanduni.ac.nz>
2024-06-25 23:42:00 +1200

    Updated preview borders + previews
    Made the borders different colours as wanted from design, and attempted to fix the dimensions and the display itself for LinkedIn and Facebook previews.

--- frontend/src/components/socials.tsx
+++ frontend/src/components/socials.tsx
@@ -8,13 +8,13 @@ import { FacebookEmbed } from 'react-social-media-embed/dist/components/embeds/F
 function Socials() {
   return (
     <div className={styles.socials}>
-        <div className={styles.socialsBorders}>
+        <div className={styles.socialsBorders1}>
             <InstagramEmbed className={styles.previews} url="https://www.instagram.com/aspa.uoa/?hl=en"/>
         </div>
-        <div className={styles.socialsBorders}>
+        <div className={styles.socialsBorders2}>
             <LinkedInEmbed className={styles.previews} url="https://www.linkedin.com/company/aspa-auckland-student-pool-association"/>
         </div>
-        <div className={styles.socialsBorders}>
+        <div className={styles.socialsBorders3}>
             <FacebookEmbed className={styles.previews} url="https://www.facebook.com/login/?next=https%3A%2F%2Fwww.facebook.com%2Faspa.uoa%2F"/>
         </div>
     </div>
