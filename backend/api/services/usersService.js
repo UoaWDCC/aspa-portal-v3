d3c79e416e784163ea3594e4a0e9e5a7fa86e0da
Kevin Cheung <48199865+nzkevc@users.noreply.github.com>
2024-06-27 13:28:56 +1200

    fix: forgot to add+commit usersService as well

--- backend/api/services/usersService.js
+++ backend/api/services/usersService.js
@@ -3,10 +3,61 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 const usersService = {
+  // GET /users
   async getAllUsers() {
     const users = await prisma.user.findMany();
     return users;
   },
+
+  // GET /users/{id}
+  async getUserById(id) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: parseInt(id),
+      },
+    });
+    return user;
+  },
+
+  // GET /users/allNames
+  async getAllUserNames() {
+    const userNames = await this.getAllUsers();
+    return userNames.map(user => `${user.firstName} ${user.lastName}`);
+  },
+
+  // GET /users/totalNumber
+  async getTotalNumberOfUsers() {
+    const users = await this.getAllUsers();
+    return users.length;
+  },
+
+  // POST /users
+  async createUser(data) {
+    const user = await prisma.user.create({
+      data,
+    });
+    return user;
+  },
+
+  // PUT /users/{id}
+  async updateUser(id, data) {
+    const user = await prisma.user.update({
+      where: {
+        id: parseInt(id),
+      },
+      data,
+    });
+    return user;
+  },
+
+  // DELETE /users/{id}
+  async deleteUserById(id) {
+    await prisma.user.delete({
+      where: {
+        id: parseInt(id),
+      },
+    });
+  }
 };
 
 export default usersService;
