db613ea25970bd1b462c9d81a93ebb63f935d2c5
Yoyo <yoyoc.pata@gmail.com>
2024-07-03 19:42:59 +1200

    Completed the Parts of Event Info Section
    Added image and changed line height of the title

--- frontend/src/ui/events/EventInfo.tsx
+++ frontend/src/ui/events/EventInfo.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import styles from './EventInfo.module.css';
+import Image from "next/image";
 
 function EventInfo(){
   const handleSubmit = (event: React.ChangeEvent<any>) => {
@@ -16,7 +17,7 @@ function EventInfo(){
             <p>Time: <span className='underline font-bold'>12:00pm-2.00pm</span></p>
             <p>Entry Fee: <span className='underline font-bold'>$6.00</span></p>
           </div>
-
+          <Image src='/Event Photo.svg' width={500} height={400} alt="Event Photo"/>
         </div>
         <button onClick={handleSubmit}>Sign Up Now!</button>
         <p>Join us for a paw-some poolside adventure at ASPA's "Pet-Pool Palooza" event! Dive into fun with your furry friends as we transform the pool deck into a pet paradise. Whether it's a playful splash with your pooch or a relaxing float with your feline friend, our pet-friendly pool day promises wagging tails and happy purrs all around. With contests, prizes, and plenty of treats, it's a tail-wagging, water-loving extravaganza you won't want to miss!</p>
