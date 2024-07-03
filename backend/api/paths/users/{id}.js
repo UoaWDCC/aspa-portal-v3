c7224ef883557cd86d55bd4186d7396daf22df6f
Kevin Cheung <48199865+nzkevc@users.noreply.github.com>
2024-07-03 18:15:57 +1200

    feat: implement {id} endpoints, apidoc and error handling

--- backend/api/paths/users/{id}.js
+++ backend/api/paths/users/{id}.js
@@ -3,18 +3,34 @@ export default function (usersService) {
     GET, PUT, DELETE
   };
 
-  // TODO: Add error handling for if user not found
   async function GET(req, res, next) {
-    res.json(await usersService.getUserById(req.params.id));
+    const user = await usersService.getUserById(req.params.id);
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).json({ message: "User not found." });
+    }
   }
 
-  // Add error handling for if user not found or malformed data
   async function PUT(req, res, next) {
-    res.json(await usersService.updateUser(req.params.id, req.body));
+    if (req.body.role) {
+      res.status(401).json({ message: "Cannot update user role." });
+    }
+    try {
+      const user = await usersService.updateUser(req.params.id, req.body);
+      if (user) {
+        res.json(user);
+      } else {
+        res.status(404).json({ message: "User not found." });
+      }
+    } catch (error) {
+      res.status(400).json({ message: "Bad Request." });
+    }
   }
 
+  // TODO: wait for implementation of deleting tickets in ticketsService.js
   async function DELETE(req, res, next) {
-    usersService.deleteUserById(req.params.id);
+    await usersService.deleteUserById(req.params.id);
     res.status(204).json({ message: "Successfully deleted!" });
   }
 
@@ -33,12 +49,92 @@ export default function (usersService) {
           schema:
             $ref: '#/definitions/User'
         404:
-          description: 'User not found in the database.'
+          description: 'Error: User not found in the database.'
           schema:
             type: 'object'
             properties:
               message:
                 type: string
+                default: 'User not found.'
+      `;
+
+  PUT.apiDoc = `
+      summary: 'Updates a specific user, given their userID.'
+      operationId: 'updateUserById'
+      parameters:
+        - name: id
+          in: path
+          description: 'The ID of the user'
+          required: true
+          type: integer
+        - in: 'body'
+          name: 'body'
+          description: 'User object to be updated'
+          required: true
+          schema:
+            type: 'object'
+            properties:
+              email:
+                type: 'string'
+              firstName:
+                type: 'string'
+              lastName:
+                type: 'string'
+              university:
+                type: 'string'
+              studentId:
+                type: 'integer'
+              upi:
+                type: 'string'
+              role:
+                type: 'string'
+              skillLevel:
+                type: 'string'
+              phoneNumber:
+                type: 'string'
+      responses:
+        200:
+          description: 'User successfully updated.'
+          schema:
+            $ref: '#/definitions/User'
+        400:
+          description: 'Error: Bad Request (likely missing required fields)'
+          schema:
+            type: 'object'
+            properties:
+              message:
+                type: string
+                default: 'Bad Request.'
+        401:
+          description: 'Error: Cannot update user role.'
+          schema:
+            type: 'object'
+            properties:
+              message:
+                type: string
+                default: 'Cannot update user role.'
+        404:
+          description: 'Error: User not found in the database.'
+          schema:
+            type: 'object'
+            properties:
+              message:
+                type: string
+                default: 'User not found.'
+      `
+
+  DELETE.apiDoc = `
+      summary: 'Deletes a specific user, given their userID.'
+      operationId: 'deleteUserById'
+      parameters:
+        - name: id
+          in: path
+          description: 'The ID of the user'
+          required: true
+          type: integer
+      responses:
+        204:
+          description: 'User successfully deleted.'
       `;
 
   return operations;
