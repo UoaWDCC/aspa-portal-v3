022af746eecbd0c58a19e296a64bb372a1159966
root <skul970@aucklanduni.ac.nz>
2024-07-16 04:22:15 +0000

    Added .subtitle and .button.icon in SignIn.module.css

--- frontend/src/ui/login/SignIn.tsx
+++ frontend/src/ui/login/SignIn.tsx
@@ -10,10 +10,15 @@ export default function SignIn() {
         await signIn("google")
       }}
     >
-      <div className="{}">
+      <div className={styles.container}>
+        <h1 className={styles.title}>
+          LOGIN
+        </h1>
         <button type="submit" className={styles.button}>
           <img src="/google-icon-PNG.png" alt="Google Icon" className={styles.icon} />
-          Login
+          <h1 className={styles.subtitle}>
+            Login
+          </h1>
         </button>
       </div>  
     </form>
