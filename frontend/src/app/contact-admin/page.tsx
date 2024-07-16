e46ea67a73cf3b0afb8e132020080d51ffc40a5e
Yoyo <xche558@aucklanduni.ac.nz>
2024-07-17 00:26:15 +1200

    Finished Initial Admin View of Contacts Page
    Created the default admin view for contacts page.

---
+++ frontend/src/app/contact-admin/page.tsx
@@ -0,0 +1,21 @@
+
+import TeamList from '@/ui/contact-admin/TeamList-admin1'
+import Image from "next/image";
+
+export default function Contact() {
+  return (
+    <div>
+      <section id="hero-banner" style={{position: 'relative', width: '100%', height: '32rem'}}>
+        <Image 
+          src="/ContactsHeroBanner.png"
+          alt="Hero banner for contacts page"
+          layout="fill"
+          objectFit='contain'
+        />
+      </section>
+      <section id="meet-the-team">
+        <TeamList/>
+      </section>
+    </div>
+  );
+}
---
+++ frontend/src/ui/contact-admin/TeamList-admin1.tsx
@@ -0,0 +1,31 @@
+import styles from './TeamList.module.css';
+import ExecCard from "./ExecCard"
+
+export default function MeetTheTeam() {
+
+  // Setup placeholder execs
+  const execs = [
+    {name: "Sky Zhang", title: "Executive", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eia pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", imgUrl: "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"}, 
+    {name: "Zen Dogra", title: "Executive", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eia pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", imgUrl: "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"},
+    {name: "Jeffery Luo", title: "Executive", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eia pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", imgUrl: "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"},
+    {name: "Amado Manaois", title: "Executive", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eia pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", imgUrl: "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"},
+    {name: "Vicki Kong", title: "Executive", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eia pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", imgUrl: "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"},
+    {name: "Jasmine Cheung", title: "Executive", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eia pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", imgUrl: "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"}
+  ];
+ 
+  return (
+      <div className={styles.container}>
+        <h1 className={styles.title}>
+          Meet The Team
+        </h1>
+        <div className={styles.content}>
+          {/* Loop through all the execs and show their respective card*/}
+          { execs.map((exec) => (
+            // Give props, exec and id
+            <ExecCard key={exec.name} exec={exec}/>
+          ))}
+        </div>
+        <button>EDIT MEMBERS</button>
+      </div>
+  )
+}
