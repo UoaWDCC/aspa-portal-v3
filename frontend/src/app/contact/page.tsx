1f9275dbe10ec448dd69e9c669ec892dcd7200c3
ylei532 <80793740+ylei532@users.noreply.github.com>
2024-07-25 23:04:15 +1200

    banners fixed + resize contact component

--- frontend/src/app/contact/page.tsx
+++ frontend/src/app/contact/page.tsx
@@ -2,16 +2,29 @@
 import TeamList from '@/ui/contact/TeamList'
 import ContactForm from '@/ui/contact/ContactForm';
 import Image from "next/image";
+import HeroBanner from "../../../public/ContactsHeroBanner.png"
 
 export default function Contact() {
   return (
     <div>
-      <section id="hero-banner" style={{position: 'relative', width: '100%', height: '32rem'}}>
+      {/* <section id="hero-banner" style={{position: 'relative', height:'32em', overflow: "hidden"}}>
         <Image 
           src="/ContactsHeroBanner.png"
           alt="Hero banner for contacts page"
-          layout="fill"
-          objectFit='contain'
+          fill
+          objectFit= "cover"
+          quality={100}
+        />
+      </section> */}
+      <section id="hero-banner" >
+        <Image 
+          src={HeroBanner}
+          alt="Hero banner for contacts page"
+          sizes="100vw"
+          style={{
+            width: '100%',
+            height: 'auto',
+          }}
         />
       </section>
       <section id="meet-the-team">
