c7b7134701315dee35e95cf3fea94fdfb1b4f271
Henry Ly <160989916+henryhlly@users.noreply.github.com>
2024-07-27 00:27:10 +1200

    Fixed conditional rendering nested page bug

---
+++ frontend/src/ui/profile-page/NavLinks.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+export default function NavLinks() {
+    const links = [
+        { name: 'Home', href: '/home' },
+        { name: 'Contact', href: '/contact' },
+        { name: 'Events', href: '/events' },
+    ]
+
+    
+}
