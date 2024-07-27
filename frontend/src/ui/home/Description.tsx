82cdf8202bc356cd6153d0b0333183a6c059aa8a
jaspeng6 <jaspeng6@gmail.com>
2024-07-28 11:55:06 +1200

    Rough content added - still have to do CSS

---
+++ frontend/src/ui/home/Description.tsx
@@ -0,0 +1,25 @@
+import styles from "./Description.module.css";
+
+export default function Description() {
+    return (
+        <div className={styles.container}>
+          <div className={styles.leftColumn}>
+            <h1 className={styles.title}>Welcome to Auckland Student Pool Association!</h1>
+            <p>
+              Leque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, Leque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, Leque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur,
+            </p>
+            <div>
+              <button>Current Events</button>
+              <p>dolor sit amet, consectetur,</p>
+            </div>
+          </div>
+          <div className={styles.rightColumn}>
+            <img src="path/to/your/image.jpg" alt="Association Image" className={styles.image}/>
+            <div className={styles.buttonContainer}>
+              <button>Register</button>
+              <button>Login</button>
+            </div>
+          </div>
+        </div>
+      );
+}
