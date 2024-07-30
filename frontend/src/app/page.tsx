7ff954a044b476e7cd58e99f383f9b23841181ec
ylei532 <80793740+ylei532@users.noreply.github.com>
2024-07-30 22:36:40 +1200

    integrated modal components from henry

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,25 @@
+import Modal from "@/ui/Modal"
+import Button from "@/ui/TestButton"
+
 export default function App() {
+  async function onDeny() {
+    'use server'
+    console.log("Deny was clicked")
+  }
+
+  async function onConfirm() {
+    'use server'
+    console.log("Confirm was clicked")
+  }
+
   return (
     <div>
-      Hi
+      <Button />
+      <Modal onConfirm={onConfirm} onDeny={null}>
+        <p>
+          You have added a new Member!
+        </p>
+      </Modal>
     </div>
   );
 }
