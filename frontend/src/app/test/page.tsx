3d7199ff5230921925a4e362048d254454abecfa
ylei532 <80793740+ylei532@users.noreply.github.com>
2024-07-31 19:21:41 +1200

    centered forms

---
+++ frontend/src/app/test/page.tsx
@@ -0,0 +1,11 @@
+import Test from '@/ui/home/Test'
+export default function Home() {
+  return (
+    <div>
+      <Test content='hello 1'/>
+      <Test content='hello 2'/>
+      <Test content='hello 3'/>
+      <Test content='hello 4'/>
+    </div>
+  );
+}
