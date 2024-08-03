04499ffae84d27920ef39fae5c91ec26d7587450
styxpienestincendies <137850839+styxpienestincendies@users.noreply.github.com>
2024-08-03 20:18:21 +1200

    feat: Updated Webpage Title for /home

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -1,14 +1,20 @@
 import PastEvents from "@/ui/PastEvents";
 import Description from "@/ui/home/Description";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "ASPA - Auckland Student Pool Association",
+  description: "ASPA Home: View ASPA Landing Page",
+};
 
 export default function Home() {
   return (
     <div>
       <section id="description">
-        <Description/>
+        <Description />
       </section>
       <section id="past-events">
-        <PastEvents/>
+        <PastEvents />
       </section>
     </div>
   );
