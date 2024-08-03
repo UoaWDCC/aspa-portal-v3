5bdc8eaa8b3893599ddc2e9e8176fa7a1a27b4d2
styxpienestincendies <137850839+styxpienestincendies@users.noreply.github.com>
2024-08-03 20:08:24 +1200

    feat: Update webpage title for /events directory

--- frontend/src/app/events/page.tsx
+++ frontend/src/app/events/page.tsx
@@ -1,11 +1,18 @@
 import Image from 'next/image';
 import HeroBanner from "../../../public/EventsHeroBanner.png"
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: "ASPA - Event",
+  description: "ASPA Events: View All the Event ASPA has to offer",
+};
+
 
 export default function Events() {
-    return (
-      <div>
+  return (
+    <div>
       <section id="hero-banner" >
-        <Image 
+        <Image
           src={HeroBanner}
           alt="Hero banner for contacts page"
           sizes="100vw"
@@ -15,7 +22,6 @@ export default function Events() {
           }}
         />
       </section>
-      </div>
-    );
-  }
-  
+    </div>
+  );
+}
