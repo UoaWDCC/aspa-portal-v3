6605c39d0ad1cf7aa66a08a3ac02d26b6e39758d
gbhu753 <gbhu753@aucklanduni.ac.nz>
2024-08-04 21:55:39 +1200

    Feat: Make footer responsive

--- frontend/src/ui/Footer.tsx
+++ frontend/src/ui/Footer.tsx
@@ -11,9 +11,12 @@ export default function Footer() {
           <p className={styles.p}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
         <div className={styles.rightcontainer}>
-          <div>
-            <Link href='/contact'><h1 className={styles.h1}>Talk To Us</h1></Link>
-            <Link href='/events'><h1 className={styles.h1}>Events</h1></Link>
+          <div className={styles.links}>
+
+            <ul>
+              <Link href='/contact'><h1 className={styles.h1}>Talk To Us</h1></Link>
+              <Link href='/events'><h1 className={styles.h1}>Events</h1></Link>
+            </ul>
           </div>
           <div className={styles.socialmedia}>
             <Image src='/discord.svg' width={60} height={60} alt="discord logo"/>
