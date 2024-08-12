860c648051c95c09803f4581975d7abbb9078997
henryhlly <lyhenry2004@gmail.com>
2024-08-12 14:39:56 +1200

    Changed modal from using searchParams to useState

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,21 +1,31 @@
+'use client'
+
 import Modal from "@/ui/Modal"
 import Button from "@/ui/TestButton"
+import { useState } from "react"
 
 export default function App() {
+
+  const [showModal, setShowModal] = useState(false);
+  const toggleModal = () => {
+    setShowModal((prev) => !prev);
+    console.log(showModal);
+  };
+
   async function onDeny() {
-    'use server'
     console.log("Deny was clicked")
+    setShowModal(false)
   }
 
   async function onConfirm() {
-    'use server'
     console.log("Confirm was clicked")
+    setShowModal(false)
   }
 
   return (
     <div>
-      <Button />
-      <Modal onConfirm={onConfirm} onDeny={null}>
+      <Button handleClick={toggleModal}/>
+      <Modal showModal={showModal} onConfirm={onConfirm} onDeny={onDeny}>
         <p>
           You have added a new Member!
         </p>
