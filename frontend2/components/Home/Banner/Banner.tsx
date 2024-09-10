75332670529e3d2227e2b6d84a380ba0de72c1b2
gbhu753 <gbhu753@aucklanduni.ac.nz>
2024-09-10 11:53:35 +0000

    Fix: Change over to string padding

--- frontend2/components/Home/Banner/Banner.tsx
+++ frontend2/components/Home/Banner/Banner.tsx
@@ -10,6 +10,10 @@ export interface BannerProps {
   groupText: string;
   titleImageSrc: string;
   titleImageAlt: string;
+  paddingTop?: string;
+  paddingBottom?: string;
+  paddingleft?: string;
+  paddingRight?: string;
 }
 
 export function Banner({
@@ -19,35 +23,41 @@ export function Banner({
     groupText,
     titleImageSrc,
     titleImageAlt,
+    paddingTop = "",
+    paddingBottom = "",
+    paddingleft = "",
+    paddingRight = ""
 }: BannerProps) {
 
   return (
-    <Grid gutter="xl">
-    <Grid.Col span={{ base: 12, sm: 6 }}>
-      <Title className={styles.title}>
-        {title}
-      </Title>
-      <Text className={styles.text}>
-        {description}
-      </Text>
-      <Group align="center">
-        <Button className={styles.button}>
-          {buttonText}
-        </Button>
-        <Text className={styles.group}>
-          {groupText}
-        </Text> 
-      </Group>
-    </Grid.Col>
-    <Grid.Col span={{ base: 12, sm: 6 }} className={styles.imageContainer}>
-      <Image 
-        src={titleImageSrc} 
-        alt={titleImageAlt} 
-        width={0} 
-        height={0} 
-        className={`${styles.image} ${styles.hoverUpDown}`}
-      />
-    </Grid.Col>
-  </Grid>
+    <Container fluid pt={paddingTop} pb={paddingBottom} pl={paddingleft} pr={paddingRight}>
+      <Grid gutter="xl">
+      <Grid.Col span={{ base: 12, sm: 6 }}>
+        <Title className={styles.title}>
+          {title}
+        </Title>
+        <Text className={styles.text}>
+          {description}
+        </Text>
+        <Group align="center">
+          <Button className={styles.button}>
+            {buttonText}
+          </Button>
+          <Text className={styles.group}>
+            {groupText}
+          </Text> 
+        </Group>
+      </Grid.Col>
+      <Grid.Col span={{ base: 12, sm: 6 }} className={styles.imageContainer}>
+        <Image 
+          src={titleImageSrc} 
+          alt={titleImageAlt} 
+          width={0} 
+          height={0} 
+          className={`${styles.image} ${styles.hoverUpDown}`}
+        />
+      </Grid.Col>
+    </Grid>
+  </Container>
   );
 }
