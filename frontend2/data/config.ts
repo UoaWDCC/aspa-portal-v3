2a6609e8f821546f2aad60d3080f5d9672eec60e
gbhu753 <gbhu753@aucklanduni.ac.nz>
2024-09-12 13:48:22 +0000

    Feat: Add temp setter for URL

--- frontend2/data/config.ts
+++ frontend2/data/config.ts
@@ -1,2 +1,6 @@
 // This need to be moved to an environment variable
-export const URL = 'http://host.docker.internal:8055';
+export let URL = 'http://host.docker.internal:8055';
+
+export const setURL = (url: string) => {
+  URL = url;
+};
