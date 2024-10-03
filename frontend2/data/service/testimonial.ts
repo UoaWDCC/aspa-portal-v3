60c303330778aabd49cbf9bac2650413da39ce54
timsinyakov <tim@DESKTOP-Q2F8A4T.>
2024-10-03 13:15:28 +1300

    added executive profile integration

--- frontend2/data/service/testimonial.ts
+++ frontend2/data/service/testimonial.ts
@@ -18,3 +18,4 @@ export async function getTestimonials(): Promise<TestimonialResponse[]> {
 
     return testimonials
 }
+
--- frontend2/data/types/testimonial.ts
+++ frontend2/data/types/testimonial.ts
@@ -12,3 +12,4 @@ export interface TestimonialResponse {
 export interface TestimonialSchema {
     Testimonials: TestimonialRequest[];
 }
+
