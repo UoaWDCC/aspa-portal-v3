5a636a63a9f99a9d1a497e5d6209865da1bf1dde
gbhu753 <gbhu753@aucklanduni.ac.nz>
2024-10-20 22:48:18 +1300

    Fix: Location of remount of directus

--- frontend2/data/config.ts
+++ frontend2/data/config.ts
@@ -1,5 +1,5 @@
 // This need to be moved to an environment variable
-export let URL = 'http://host.docker.internal:8055';
+export let URL = 'http://localhost:8055/';
 
 export const setURL = (url: string) => {
   URL = url;
