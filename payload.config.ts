0a83b4e23ba93e87a2a3207823c624b540f2d73d
gbhu753 <gbhu753@aucklanduni.ac.nz>
2025-06-04 13:27:48 +0000

    Chore: Add config for payload

---
+++ payload.config.ts
@@ -0,0 +1,26 @@
+import sharp from 'sharp'
+import { lexicalEditor } from '@payloadcms/richtext-lexical'
+import { postgresAdapter } from '@payloadcms/db-postgres'
+import { buildConfig } from 'payload'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
+
+
+export default buildConfig({
+  editor: lexicalEditor(),
+  collections: [],
+  secret: process.env.PAYLOAD_SECRET || '',
+  typescript: {
+    outputFile: path.resolve(dirname, 'payload-types.ts'),
+  },
+  db: postgresAdapter({
+    pool: {
+      connectionString: process.env.DATABASE_URI || '',
+    },
+  }),
+  sharp,
+  plugins: [],
+})
