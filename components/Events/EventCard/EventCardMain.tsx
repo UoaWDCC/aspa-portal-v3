eda9c2727e8e9f0046c069ac1aa703739a4c69ab
Henry Ly <160989916+henryhlly@users.noreply.github.com>
2025-07-17 02:52:51 +0000

    Finished primary event, missing sign up button

--- components/Events/EventCard/EventCardMain.tsx
+++ components/Events/EventCard/EventCardMain.tsx
@@ -1,16 +1,43 @@
-import { Card, Image } from '@mantine/core';
+import { Flex, Card, Image, Text, rgba } from '@mantine/core';
+import { IconFlagExclamation } from '@tabler/icons-react';
 
 export function EventCardMain() {
+    // Temp colors, waiting for universal color
+    // Need website font as well
+    // Temp image, waiting for universal image
+    const bg_color = rgba('#717882', 1);
+    const font_color = rgba('#FFFFFF', 1);
+
     return (
-        <Card padding="lg" style={{ display: 'flex', flexDirection: 'row' }}>
+        <Flex direction="row" align="center" justify="flex-start" bg={bg_color}>
             <Image
-                src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
-                width={60}
-                height={60}
+                src="https://www.bartonmcgill.co.nz/wp-content/uploads/2023/12/51S4Ow-YSgL-_AC_SL1500_.jpg"
+                w="20%"
+                h="20%"
                 alt="Example"
             />
-            <h2>Event Name</h2>
-            <p>Description of the event goes here. It can be a brief overview of what the event is about.</p>
-        </Card> 
+
+            <Flex direction="column" m="md" justify="space-around">
+                {/* Title of event */}
+                <Text size="xl" tt="uppercase" mt="md" mb="md" c={font_color}>
+                    Casual Night @ AKL CBD
+                </Text>
+
+                {/* Event date, price and location */}
+                <Flex direction="row" justify="space-between" align="center" gap="auto" mt="md" mb="md" c={font_color}>
+                    <Text size="md">TUES & THUR 6:30pm-8:00pm</Text>
+                    <Text>$6</Text>
+                    <Text>Orange Pool Club [9 City Road]</Text>
+                </Flex>
+
+                {/* Event description */}
+                <Text size="md" mt="xs" c={font_color}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                    dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+                    non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </Text>
+            </Flex>
+        </Flex> 
     );
 }
