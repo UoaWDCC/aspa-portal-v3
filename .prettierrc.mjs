f4e0e6855c9fe52e3c8ec1a4b5252ba0d9691411
gbhu753 <gbhu753@aucklanduni.ac.nz>
2025-07-20 12:19:02 +0000

    Chore: Update prettier

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,27 +1,8 @@
-/** @type {import("@ianvs/prettier-plugin-sort-imports").PrettierConfig} */
+/** @type {import("prettier").Config} */
 const config = {
   printWidth: 100,
   singleQuote: true,
   trailingComma: 'es5',
-  plugins: ['@ianvs/prettier-plugin-sort-imports'],
-  importOrder: [
-    '.*styles.css$',
-    '',
-    'dayjs',
-    '^react$',
-    '^next$',
-    '^next/.*$',
-    '<BUILTIN_MODULES>',
-    '<THIRD_PARTY_MODULES>',
-    '^@mantine/(.*)$',
-    '^@mantinex/(.*)$',
-    '^@mantine-tests/(.*)$',
-    '^@docs/(.*)$',
-    '^@/.*$',
-    '^../(?!.*.css$).*$',
-    '^./(?!.*.css$).*$',
-    '\\.css$',
-  ],
   overrides: [
     {
       files: '*.mdx',
