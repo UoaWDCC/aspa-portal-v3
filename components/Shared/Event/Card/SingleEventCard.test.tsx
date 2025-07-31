e82ce3eaff5cb76d916a042773aefeb38999db7a
Olivia Kusuma <okus245@aucklanduni.ac.nz>
2025-07-31 06:11:20 +0000

    change the file name fir the test case

--- components/Shared/Event/Card/SingleEventCard.test.tsx
+++ components/Shared/Event/Card/SingleEventCard.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
-import { EventCard } from "./singleeventcard";
+import { EventCard } from "./SingleEventCard";
 import { useRouter } from "next/navigation";
 import "@testing-library/jest-dom";
 
