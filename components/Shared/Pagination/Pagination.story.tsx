c4c1f6f31a9c6dab1209a87e14f1172d68b89b8c
nath6428 <nath6428@users.noreply.github.com>
2025-08-11 04:22:30 +0000

    Chore: Prettify code

--- components/Shared/Pagination/Pagination.story.tsx
+++ components/Shared/Pagination/Pagination.story.tsx
@@ -1,8 +1,8 @@
-import Pagination from "./Pagination";
-import { useState } from "react";
+import Pagination from './Pagination';
+import { useState } from 'react';
 
 export default {
-  title: "Shared/Pagination",
+  title: 'Shared/Pagination',
   component: Pagination,
 };
 
@@ -11,11 +11,11 @@ export const Default = () => {
 
   return (
     <Pagination
-    currentPage={page}
-    onPageChange={setPage}
-    pageSize={10}
-    collectionLength={21}
-    collectionName="Events"
+      currentPage={page}
+      onPageChange={setPage}
+      pageSize={10}
+      collectionLength={21}
+      collectionName="Events"
     />
-  )
+  );
 };
